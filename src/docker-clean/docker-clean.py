36576a004e90b0fb639fc1afc0bf475db3874c4d
jmreis <jairmsreis@gmail.com>
2021-09-17 16:09:13 -0300

    Adicionando conteúdo de Readme

--- src/docker-clean/docker-clean.py
+++ src/docker-clean/docker-clean.py
@@ -1,21 +1,45 @@
 #!/usr/bin/env python
-import sys
+import os
 
+
+print(
+	"""\033[1;94m
+	⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠛⠛⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+	⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⣀⣀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+	⣿⣿⣿⣿⣿⣿⣿⠀⠀⢸⡇⠀⠀⡇⠀⠀⢸⣿⣿⣿⣿⣿⡟⠻⣿⣿⣿⣿⣿⣿
+	⣿⣿⣿⡿⠿⠿⣿⠶⠶⢾⡷⠶⠶⡷⠶⠶⢾⠿⠿⠿⣿⣿⠀⠀⠈⣿⣿⣿⣿⣿
+	⣿⣿⣿⡇⠀⠀⣿⠀⠀⢸⡇⠀⠀⡇⠀⠀⢸⠀⠀⠀⣿⣿⡀⠀⠀⠀⠀⠀⢉⣿
+	⣿⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠀⠀⢀⣀⣠⣴⣿⣿
+	⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿
+	⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿
+	⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿
+	⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+	⣿⣿⣿⣿⣿⣷⣶⣦⣤⣤⣤⣤⣤⣤⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+
+		DOCKER-CLEAN
+		by Jair Reis
+
+	\033[0;0m""")
 
 # Listing containers
 os.system("docker ps -a -q")
 # cleaning containers
-print("Cleaning containers....................................")
+print("\033[1;94mCleaning containers....................................\033[0;0m")
 os.system("docker rm $(docker ps -a -q)")
 #docker rm $(docker ps -a -q) -f(docker rm $(docker ps -a -q) -f)
 
 # Listing images
 os.system("docker images -g")
 # Cleaning images
-print("Cleanint images........................................")
+print("\033[1;94mCleanint images........................................\033[0;0m")
 os.system("docker rmi $(docker images -q)")
 #docker rmi $(docker images -q) -f(docker rmi $(docker images -q) -f)
 
+# Cleaning volumes
+print("\033[1;94mCleaning volumes.......................................\033[0;0m")
+os.system("docker volume rm $(docker volume ls -q)")
+
+
 # List os images
-print("List of docker images..................................")
+print("\033[1;94mList of docker images..................................\033[0;0m")
 os.system("docker images")
